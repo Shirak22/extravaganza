b82a4426bdc57e678253a0bd65e00b336fe92e22
Shirak22 <116073530+Shirak22@users.noreply.github.com>
2023-11-04 00:02:58 +0100

    fixing some animations

--- app/js/animations.js
+++ app/js/animations.js
@@ -13,9 +13,7 @@ logo.innerHTML = letters;
 const logo_timeline = anime.timeline({
         autoplay:true,
 });
-        
-const primaryColor =  '#d97706'; 
-const normalColor =  '#ffffffbc'; 
+
 
         logo_timeline.add({
                 targets:'#title span',
@@ -72,7 +70,7 @@ const normalColor =  '#ffffffbc';
 
 
 
-//        hamburger menu 
+// hamburger menu 
 const nav_list_animation = anime({
         targets:'.navigation_list > * ',
         translateX: [350,0],
@@ -83,26 +81,73 @@ const nav_list_animation = anime({
         autoplay:false,
 });
  
-const hamburger_menu = document.querySelector('.nav_hamburger-btn'); 
-const menu_overlay = document.querySelector('.menu_overlay'); 
+const hamburger_menu = document.querySelector('.nav_hamburger-btn');
+const menu_overlay = document.querySelector('.menu_overlay');
 
-hamburger_menu.addEventListener('click',()=> {
-    if(menu_overlay.getAttribute('aria-expanded') == 'true'){
-        menu_overlay.setAttribute('aria-expanded','false')
-}else {
-    menu_overlay.setAttribute('aria-expanded','true')
-    
-}
-    menu_overlay.classList.toggle('open_menu');
-    if(document.querySelector('.menu_overlay').getAttribute('aria-expanded') == 'true'){
-        nav_list_animation.play();
-}else {
-        console.log('it is false');
+hamburger_menu.addEventListener('click', () => {
+        if (menu_overlay.getAttribute('aria-expanded') == 'true') {
+                menu_overlay.setAttribute('aria-expanded', 'false')
+        } else {
+                menu_overlay.setAttribute('aria-expanded', 'true')
 
-    
-}
+        }
+
+        menu_overlay.classList.toggle('open_menu');
+        hamburger_menu.classList.toggle('open');
+        if (document.querySelector('.menu_overlay').getAttribute('aria-expanded') == 'true') {
+                nav_list_animation.play();
+        } else {
+                console.log('it is false');
+
+        }
 })
 
 
+//card slider
+const card = Array.from(document.querySelectorAll('.card_slider-card')); 
+card.forEach(card => {
+        card.addEventListener('click',()=> {
+                card.classList.toggle('card_slider-card-hover');
+
+        });
+}); 
 
 
+
+
+
+
+
+
+// card gallery 
+        const card_gallery = Array.from(document.querySelectorAll('.card_gallery-card')); 
+        card_gallery.forEach(card  => {
+                card.addEventListener('mouseover',() => {
+                        card.classList.add('card_gallery-hover');
+                })
+
+                card.addEventListener('mouseout',() => {
+                        card.classList.remove('card_gallery-hover');
+                 })
+                
+        });
+
+
+
+//Pricing 
+
+        const animate_packages = anime.timeline();
+
+        animate_packages.add({
+                targets:'.pricing',
+                opacity:[0,1],
+                duration:2000,
+                easing:'easeOutSine',
+        }).add({
+                targets:'.pricing_package',
+                translateX:[-50,0],
+                opacity:[0,1],
+                duration:2000,
+                delay: anime.stagger(250),
+                easing:'easeOutSine',
+        },500)
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 import  "./animations.js";
 
 
+    const modal =  document.querySelector('.sale_modal-container'); 
+
+const observer = new IntersectionObserver(entries => {
+  
+        entries.forEach((entry) => {
+            if(entry.isIntersecting){
+                console.log(entry);
+            }else {
+                console.log(entry);
+            }
+        })
+
+}); 
+
+
+
+
+window.addEventListener('click', ()=>{
+    
+    modal.classList.toggle('show');
+}
+)
